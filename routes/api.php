08716ae71bad3be8fa07f4f22e8e628381640333
Paulosergiobp <sistema1@engepecas.com.br>
2021-06-01 14:55:50 -0300

    LIMPEZA DE API INTEGRACAO

--- routes/api.php
+++ routes/api.php
@@ -13,45 +13,14 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-/*
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-*/
 
 Route::post('login', 'Auth\AuthenticateController@authenticate');
 Route::post('login-refresh', 'Auth\AuthenticateController@refreshToken');
 Route::get('me', 'Auth\AuthenticateController@getAuthenticatedUser');
 
-//Route::resource('purchaseHistOrders', 'PurchaseHistOrdersAPIController');    
-
-
-//Route::resource('purchaseHistIncomingInvoices', 'PurchaseHistIncomingInvoiceAPIController');
-
-
-//Route::middleware('auth:api')->get('/user', function (Request $request) {
-//    return $request->user();
-//});
-
-//Route::resource('home', 'HomeController');
-//Route::group(['middleware' => ['apiJwt']], function () {
-//   Route::group(['namespace' => 'Api', 'middleware' => 'auth:api'], function () {
-//    Route::group(['middleware' => ['api']], function () {
-
-
-
-
 #    Route::group(['namespace' => 'Api', 'middleware' => 'auth:api','scheme' => 'https'], function () {
 
 Route::group(['namespace' => 'Api', 'middleware' => 'auth:api'], function () {
-
-
-    Route::get('H100/{id}/H101', 'H100APIController@H101');
-    Route::get('H100/{D009_Id},{Status}/getSaldo', 'H100APIController@getSaldo');
-    Route::resource('H100', 'H100APIController');  
-
-    Route::get('H101/{id}/H100', 'H101APIController@H100');
-    Route::resource('H101', 'H101APIController');
 
     Route::get('purchaseHistOrders/{T011_Id}/purchaseHistIncomingInvoicesJoin', 'PurchaseHistOrdersAPIController@purchaseHistIncomingInvoicesJoin');
     Route::get('purchaseHistOrders/{T012_Id},{Status}/getpurchaseHistOrdersItens', 'PurchaseHistOrdersAPIController@getpurchaseHistOrdersItens');
